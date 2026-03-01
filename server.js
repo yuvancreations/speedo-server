2dc246da5021467b7621660e995658d541e5bd49
yuvancreations <yuvancreationshrd@gmail.com>
2026-03-01 16:12:35 +0530

    Added PhonePe OAuth token route

--- server.js
+++ server.js
@@ -16,3 +16,25 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+const axios = require("axios");
+
+app.get("/get-token", async (req, res) => {
+  try {
+    const response = await axios.post(
+      "https://api.phonepe.com/apis/identity-manager/v1/oauth/token",
+      {
+        client_id: process.env.SU2510011750401548106040,
+        client_secret: process.env.76108613-904e-486e-ac7f-e8a149736bfc,
+        grant_type: "client_credentials",
+      }
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({
+      message: "Token generation failed",
+      error: error.response?.data || error.message,
+    });
+  }
+});
